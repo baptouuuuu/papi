f0baeed276ee843c34c56f6466535f25dcda9d47
Baptiste MAES <baptistem1720@gmail.com>
2023-11-05 21:29:14 +0100

    Question 7

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -5,13 +5,39 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\VeloType;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\AjoutVélo;
+use App\Entity\Velo;
+use Doctrine\ORM\EntityManagerInterface;
+
 
 class BaseController extends AbstractController
 {
-    #[Route('/base', name: 'app_base')]
+    #[Route('/', name: 'app_accueil')]
     public function index(): Response
     {
         return $this->render('base/index.html.twig', [
+        ]);
+    }
+    #[Route('/AjoutVélo', name: 'app_AjoutVélo')]
+    public function AjoutVélo(Request $request, EntityManagerInterface $em): Response
+    {
+        $AjoutVélo = new Velo();
+        $form = $this->createForm(VeloType::class, $AjoutVélo);
+
+        if($request->isMethod('POST')){
+            $form->handleRequest($request);
+            if ($form->isSubmitted()&&$form->isValid()){
+                $em->persist($AjoutVélo);
+                $em->flush();
+                $this->addFlash('notice','Message envoyé');
+                return $this->redirectToRoute('app_AjoutVélo');         
+            }
+        }
+           
+        return $this->render('base/AjoutVélo.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
---
+++ src/Entity/Velo.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VeloRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: VeloRepository::class)]
+class Velo
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 30)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $prix = null;
+
+    #[ORM\Column(length: 1000)]
+    private ?string $batterie = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrix(): ?string
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(string $prix): static
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getBatterie(): ?string
+    {
+        return $this->batterie;
+    }
+
+    public function setBatterie(string $batterie): static
+    {
+        $this->batterie = $batterie;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
